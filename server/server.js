81519bb0c618bc77b1f9ce7fafcbe73934f6429b
Daniel <daniel_ye.nz@hotmail.com>
2022-12-31 01:12:09 -0800

    port error fix

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -46,4 +44,4 @@ app.post('/', async(req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(5000, () => console.log(`Server is running on port http://localhost:5000`));
